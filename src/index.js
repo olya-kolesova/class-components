074993fca4369044ad0907eb8ff7516bb8c9aca2
Olga Kolesova <olka.kolesova@gmail.com>
2021-12-04 16:50:56 +0000

    changed to class component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class UserForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>Enter a username:</label>
+        <input/>
+      </form>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<UserForm />, document.querySelector('#root'));
